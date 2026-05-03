969925ecaca1893565b40c67108ed655402baba4
Tobiáš Radkovský <toby.radkovsky@gmail.com>
2026-05-03 12:59:21 +0200

    landing page sketch

---
+++ website/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os, logging
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+
+def create_app(): 
+    logger = logging.getLogger("website") # --> has to be the name of the folder its in 
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("{asctime} - {levelname}: {message}", style="{", datefmt="%Y-%m-%d %H:%M"))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    
+    app = Flask(__name__, template_folder='templates', static_folder='../static')
+
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key')
+    
+    '''database_url = os.environ.get('DATABASE_URL', 'sqlite:///dev.db')
+    if database_url.startswith('postgres://'):
+        database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        "pool_pre_ping": True,  # drops stale connections automatically
+        "pool_size": 5,
+        "max_overflow": 10,
+    }
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False    
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'  # redirect here if not logged in'''
+
+    '''@login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))'''
+
+    from .views import view
+    app.register_blueprint(view, url_prefix='/')
+
+    '''with app.app_context():
+        db.create_all()'''
+    logger.info("app created")
+    return app
---
+++ website/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+import logging
+
+view = Blueprint('view', __name__)
+logger = logging.getLogger(__name__)
+
+@view.route('/')
+def home():
+    return render_template("base.html")
